5bf5ed94ca562b3ea593f0a2d4a931a8d0386bc7
Sneha-mittal-774 <mittalsneha774@gmail.com>
2024-03-12 14:03:31 +0530

    commit file

--- giteg.java
+++ giteg.java
@@ -1,6 +1,8 @@
 public class giteg {
     public static void main(String args[]){
         System.out.println("hello");
+        System.out.println("hii");
+        System.out.println("hieei");
     }
     
 }
